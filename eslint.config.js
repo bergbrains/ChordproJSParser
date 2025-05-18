a57885e4e8648153a49095d8ea6931142d6603e6
Eric Berg <eberg@bergbrains.com>
2025-05-18 11:11:46 -0400

    Updated pre-commit...add eslint and local test..working, but tests failing

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,28 @@
-// .eslintrc.js
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
+// eslint.config.js
+import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
+
+export default [
+  eslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        browser: true,
+        es2021: true,
+        node: true,
+        jest: true,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      indent: ["error", 2],
+      "comma-dangle": ["error", "never"],
+    },
   },
-  extends: ["eslint:recommended", "prettier"],
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: "module",
-  },
-  rules: {
-    "no-unused-vars": "warn",
-    "no-console": "warn",
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    indent: ["error", 2],
-    "comma-dangle": ["error", "never"],
-  },
-};
+];
