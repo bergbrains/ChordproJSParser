a037d87904f095694c8b52b71041850f1b6bd467
Eric Berg <eric.berg@cooktech.us>
2025-11-10 13:52:19 -0500

    Fix rollup and some package.json issues...sync latest dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { terser } from "rollup-plugin-terser";
+import  terser from "@rollup/plugin-terser";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
@@ -20,6 +20,7 @@ export default {
   plugins: [
     resolve(),
     commonjs(),
+    terser(),
     postcss({
       extract: "dist/chordprojs.min.css",
       minimize: true,
